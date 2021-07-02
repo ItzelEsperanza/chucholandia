385f1735259145826969e1d4e2d06ac760e9fde8
Itzel Klippstein <itzel.klippstein7210@alumnos.udg.mx>
2021-07-02 03:06:46 -0500

    Inicio/CSS tterminados creo

--- resources/views/clientes/welcome.blade.php
+++ resources/views/clientes/welcome.blade.php
@@ -4,6 +4,40 @@
               Inicio
 </h2>
 
-            
+<h4 class="mb-4 text-lg font-semibold text-green-600 dark:text-green-300">
+             Veterinaria Chucholandia
+            </h4>
+<a href="https://i.pinimg.com/564x/e9/72/ef/e972ef1ec07485bb0d807fceca6d2715.jpg"></a>
+
+ <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
+            <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
+                Misión
+                  </p>
+
+              <p class="text-sm text-gray-600 dark:text-gray-400">
+              Satisfacer los requerimientos y expectativas en el marco de la salud veterinaria,
+              logrando así, posicionarnos como una clínica destacada por un completo cubrimiento de servicios para la salud,
+              bienestar de nuestras mascotas, desde la ética humana profesional comprometida y responsable de calidad con el fin de brindar
+              la mejor alternativa en atención especializada para peludos pacientes.
+               <br>
+              </p>
+            </div>
+
+<div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
+            <p class="text-lg font-semibold text-gray-700 dark:text-gray-200">
+               Visión
+                  </p>
+
+              <p class="text-sm text-gray-600 dark:text-gray-400">
+             Ofrecer la mejor opción en medicina veterinaria para preservar la salud de nuestras mascotas junto con la tranquilidad de sus
+             familias, ofreciendo servicios especializados de calidad en el marco de la salud veterinaria, cuidado y bienestar de las mascotas;
+             nuestro valor agregado es trabajar día a día comprometidos para lograr total cobertura a las necesidades de nuestros pacientes,
+             mediante un equipo profesional capacitado, la infraestructura e instrumentación adecuada de alta tecnología que permitiré optimizar
+             nuestra labor.
+               <br>
+              </p>
+            </div>
+
+
 @endsection
 
